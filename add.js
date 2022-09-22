ef14978994c86e4d5137fd7dacbfd090ddb9d88d
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-09-23 00:30:04 +0100

    Understand Problem, Explore Examples

---
+++ add.js
@@ -0,0 +1,16 @@
+// ***************************************
+// Write a function that adds two numbers
+// ***************************************
+
+// 1. Can I restate the problem in my own words
+"Sum of two numbers"
+// 2. What are the inputs
+"The two numbers x and y as integers or floats"
+// 3. What are the outputs of the solution
+"A number: sum of x and y"
+// 4. Can the outputs be determined but the inputs and what are the edge cases.
+"What if onlu one input is provided and which one"
+"Whats the solution for large numbers"
+"Wht if input is a string"
+"..."
+// 5. How do i label the important pieces of the data
